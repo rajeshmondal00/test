b07296860a9640858a4a65c6161848c44cd7c68c
Rajesh Mondal <xmxaxmax4@gmail.com>
2023-06-11 21:12:11 +0530

    first commit

---
+++ users/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+   class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields + ('age',)
+
+class CustomUserChangeForm(UserChangeForm):
+   class Meta:
+        model = CustomUser
+        fields = UserChangeForm.Meta.fields
